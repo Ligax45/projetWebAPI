0fa46571db3b74a4800a2d4f09e421662661cc45
Ligax45 <clem-du-45@live.fr>
2024-04-11 21:13:21 +0200

    mise a jour

--- ProjetWebApi/WebApi_Front/.eslintrc.cjs
+++ ProjetWebApi/WebApi_Front/.eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +15,19 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/explicit-function-return-type': 'warn', // Require explicit return types for functions.
+    '@typescript-eslint/no-unused-vars': 'error', // Report unused variables.
+    '@typescript-eslint/no-explicit-any': 'warn', // Avoid using 'any'.
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }], // Prefer .tsx extension for React files with JSX.
+    'react/prop-types': 'off', // Disable prop-types as you're using TypeScript.
+    //'react/jsx-props-no-spreading': 'warn', // Warn on props spreading.
+    'prettier/prettier': 'error', // Report Prettier formatting errors as ESLint errors.
+    'react/react-in-jsx-scope': 'off', // Disable as React 17+ doesn't require `React` to be in scope for JSX.
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
   },
-}
+};
---
+++ ProjetWebApi/WebApi_Front/.prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  trailingComma: 'all', // Utilise des virgules en fin de ligne dans tous les contextes. Remplacez par 'es5' si vous voulez des virgules uniquement là où c'était valide en ES5 (c'est-à-dire dans les objets et les tableaux, mais pas dans les paramètres ou les arguments des fonctions).
+  tabWidth: 2, // Utilise un espacement de 2 pour les tabulations.
+  semi: true, // Utilise un point-virgule à la fin des instructions.
+  singleQuote: true, // Utilise des guillemets simples au lieu de doubles.
+  endOfLine: 'auto', // Gère automatiquement les fins de ligne en fonction de l'environnement, évitant les problèmes de compatibilité entre différents systèmes d'exploitation.
+  arrowParens: 'always', // Encadre toujours les paramètres des fonctions fléchées avec des parenthèses.
+};
