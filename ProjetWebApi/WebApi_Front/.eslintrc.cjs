35a7e16efbf8acc6e8e5e865336d06cdd5dfa4b0
Clément Farina <122206754+Ligax45@users.noreply.github.com>
2024-04-11 22:44:47 +0200

    pc2

--- ProjetWebApi/WebApi_Front/.eslintrc.cjs
+++ ProjetWebApi/WebApi_Front/.eslintrc.cjs
@@ -4,6 +4,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
--- ProjetWebApi/WebApi_Front/src/App.tsx
+++ ProjetWebApi/WebApi_Front/src/App.tsx
@@ -1,10 +1,13 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Teste</h1>
+        <h2>pc2</h2>
+      </div>
+    </>
   );
 }
 
